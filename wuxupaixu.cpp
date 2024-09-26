b2f7f5e6332a1f95d79330edf988de18a2614d06
lu491 <2878066930@qq.com>
2024-09-26 23:15:43 +0800

    Update wuxupaixu.cpp

--- wuxupaixu.cpp
+++ wuxupaixu.cpp
@@ -52,15 +52,15 @@ void update(DynamicArray *array, int position, int newValue) {
 }  
   
 void remove(DynamicArray *array, int position) {  
-    // 你的remove函数实现已经正确  
+  
 }  
   
 void mix(DynamicArray *array) {  
-    // 你的mix函数实现已经正确  
+     
 }  
   
 int min(const DynamicArray *array) {  
-    // 你的min函数实现已经正确  
+     
 }  
   
 int partition(int arr[], int left, int right) {  
@@ -81,7 +81,7 @@ int partition(int arr[], int left, int right) {
 }  
   
 int Quick_Select(int arr[], int left, int right, int k) {  
-    // 你的Quick_Select函数实现已经正确  
+   
 }  
   
 int main() {  
@@ -90,23 +90,22 @@ int main() {
     DynamicArray myArray;  
     initDynamicArray(&myArray, 5);  
   
-    // 插入一些元素  
+  
     insert(&myArray, 3);  
     insert(&myArray, 1);  
     insert(&myArray, 4);  
     insert(&myArray, 1);  
     insert(&myArray, 5);  
   
-    // 打印原始数组  
+    
     printDynamicArray(&myArray);  
   
-    // 更新位置为2的元素为2  
     update(&myArray, 2, 2);  
     printf("After updating element at position 2 to 2: ");  
     printDynamicArray(&myArray);  
   
-    // 接下来的操作与你的代码相同  
-    // ...  
+   
+
   
     return 0;  
 }
